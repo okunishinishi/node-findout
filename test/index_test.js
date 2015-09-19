99e50880c21e13955c436354a18c62de66437c75
okunishinishi <okunishitaka.com@gmail.com>
2015-09-20 06:52:12 +0900

    Version incremented to 1.2.2

---
+++ test/index_test.js
@@ -0,0 +1,16 @@
+/**
+ * Test for index.js
+ * Runs with nodeunit.
+ */
+
+"use strict";
+
+var index = require('../lib/index.js');
+
+exports['Eval properties.'] = function(test){
+    Object.keys(index).forEach(function(key){
+        test.ok(key);
+        test.ok(index[key]);
+    });
+    test.done();
+};
